5a288c4a3fddd14f3b3f19e10c4700962438e5de
Soumo Sarkar <soumsarkar297@gmail.com>
2024-11-17 09:53:45 +0530

    Add 'getAllUsers' controller to the user route

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { protectRoute } from "../middlewares/auth.middleware";
+import { getAllUsers } from "../controllers/user.controller";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("User route with GET method");
-});
+router.get("/", protectRoute, getAllUsers);
+// TODO: getMessages route
 
 export default router;
